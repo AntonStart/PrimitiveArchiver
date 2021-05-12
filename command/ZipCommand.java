a07b4cb31a17831f9b7c758d3711e56b77f989a2
Pozdnyakov Anton <annihilator61@mail.ru>
2021-05-12 22:22:11 +0300

    Full implementation of the ZipCreateCommand class

--- command/ZipCommand.java
+++ command/ZipCommand.java
@@ -1,4 +1,17 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command{
+   public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь файла архива: \t");
+        String fullPathString = ConsoleHelper.readString();
+        Path fullPath = Paths.get(fullPathString);
+        ZipFileManager zipFileManager = new ZipFileManager(fullPath);
+        return zipFileManager;
+    }
 }
